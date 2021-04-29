e1a5667dd615faf339549f22fab13e643fcd3836
Ben Ashbaugh <ben.ashbaugh@intel.com>
2021-04-28 23:08:59 -0700

    copybuffer sample is working

--- samples/python/01_copybuffer/copybuffer.py
+++ samples/python/01_copybuffer/copybuffer.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
+import numpy as np
 import pyopencl as cl
 import argparse
 
-gwx = 1024 * 1024
+#gwx = 1024 * 1024
+gwx = 16
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -17,24 +19,32 @@ if __name__ == "__main__":
     platforms = cl.get_platforms()
     print('Running on platform: ' + platforms[platformIndex].get_info(cl.platform_info.NAME))
 
-    devices = platforms[platformIndex].get_device()
+    devices = platforms[platformIndex].get_devices()
     print('Running on device: ' + devices[deviceIndex].get_info(cl.device_info.NAME))
 
-    context = cl.Context(devices[deviceIndex])
+    context = cl.Context([devices[deviceIndex]])
     commandQueue = cl.CommandQueue(context, devices[deviceIndex])
 
-    deviceMemSrc = cl.Buffer(context, cl.mem_flags.ALLOC_HOST_PTR, )
+    deviceMemSrc = cl.Buffer(context, cl.mem_flags.ALLOC_HOST_PTR, gwx * np.uint32().itemsize)
+    deviceMemDst = cl.Buffer(context, cl.mem_flags.ALLOC_HOST_PTR, gwx * np.uint32().itemsize)
 
-for platform in cl.get_platforms():
-    print("Platform:")
-    print("        Name:           " + platform.get_info(cl.platform_info.NAME))
-    print("        Vendor:         " + platform.get_info(cl.platform_info.VENDOR))
-    print("        Driver Version: " + platform.get_info(cl.platform_info.VERSION))
-    for i, device in enumerate(platform.get_devices()):
-        print("Device[{}]:".format(i))
-        print("        Type:           " + cl.device_type.to_string(device.get_info(cl.device_info.TYPE)))
-        print("        Name:           " + device.get_info(cl.device_info.NAME))
-        print("        Vendor:         " + device.get_info(cl.device_info.VENDOR))
-        print("        Device Version: " + device.get_info(cl.device_info.VERSION))
-        print("        Driver Version: " + device.get_info(cl.device_info.DRIVER_VERSION))
-    print()
+    mapped_src, event = cl.enqueue_map_buffer(commandQueue, deviceMemSrc, cl.map_flags.WRITE_INVALIDATE_REGION, 0, gwx, np.uint32)
+    with mapped_src.base:
+        for i in range(gwx):
+            mapped_src[i] = i
+
+    cl.enqueue_copy(commandQueue, deviceMemDst, deviceMemSrc)
+
+    mapped_dst, event = cl.enqueue_map_buffer(commandQueue, deviceMemDst, cl.map_flags.READ, 0, gwx, np.uint32)
+    with mapped_dst.base:
+        mismatches = 0
+        for i, val in enumerate(mapped_dst):
+            if val != i:
+                if mismatches < 16:
+                    print('Mismatch!  dst[{}] == {}, want {}'.format(i, val, i))
+                mismatches = mismatches + 1
+        if mismatches != 0:
+            print('Error: Found {} mismatches / {} values!!!'.format(mismatches, gwx))
+        else:
+            print('Success.')
+
