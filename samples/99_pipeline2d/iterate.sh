019df17da010cfa85c51d6966f0b9957b691ad8d
Ben Ashbaugh <ben.ashbaugh@intel.com>
2021-06-08 22:33:22 -0700

    add scripts for 2d versions

---
+++ samples/99_pipeline2d/iterate.sh
@@ -0,0 +1,8 @@
+cliloader -dv -q ../install/RelWithDebInfo/pipeline2d 
+cliloader -dv -q ../install/RelWithDebInfo/pipeline2d -t 16
+cliloader -dv -q ../install/RelWithDebInfo/pipeline2d -t 32
+cliloader -dv -q ../install/RelWithDebInfo/pipeline2d -t 64
+cliloader -dv -q ../install/RelWithDebInfo/pipeline2d -t 128
+cliloader -dv -q ../install/RelWithDebInfo/pipeline2d -t 256
+cliloader -dv -q ../install/RelWithDebInfo/pipeline2d -t 512
+cliloader -dv -q ../install/RelWithDebInfo/pipeline2d -t 1024
---
+++ samples/99_pipeline2d/iterate_uncached.sh
@@ -0,0 +1,8 @@
+cliloader -dv -q ../install/RelWithDebInfo/pipeline2d --uncached 
+cliloader -dv -q ../install/RelWithDebInfo/pipeline2d --uncached -t 16
+cliloader -dv -q ../install/RelWithDebInfo/pipeline2d --uncached -t 32
+cliloader -dv -q ../install/RelWithDebInfo/pipeline2d --uncached -t 64
+cliloader -dv -q ../install/RelWithDebInfo/pipeline2d --uncached -t 128
+cliloader -dv -q ../install/RelWithDebInfo/pipeline2d --uncached -t 256
+cliloader -dv -q ../install/RelWithDebInfo/pipeline2d --uncached -t 512
+cliloader -dv -q ../install/RelWithDebInfo/pipeline2d --uncached -t 1024
