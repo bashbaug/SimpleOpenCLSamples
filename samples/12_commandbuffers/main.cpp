8460935316a9bb64ed01f59bfdccd1e244d90511
Ashbaugh <ben.ashbaugh@intel.com>
2021-11-16 16:56:19 -0800

    add a basic command buffer sample

---
+++ samples/12_commandbuffers/main.cpp
@@ -0,0 +1,187 @@
+/*
+// Copyright (c) 2019-2021 Ben Ashbaugh
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+*/
+
+#include <popl/popl.hpp>
+
+#include <CL/opencl.hpp>
+
+const size_t    gwx = 1024*1024;
+
+static const char kernelString[] = R"CLC(
+kernel void CopyBuffer( global uint* dst, global uint* src )
+{
+    uint id = get_global_id(0);
+    dst[id] = src[id];
+}
+)CLC";
+
+int main(
+    int argc,
+    char** argv )
+{
+    int platformIndex = 0;
+    int deviceIndex = 0;
+
+    {
+        popl::OptionParser op("Supported Options");
+        op.add<popl::Value<int>>("p", "platform", "Platform Index", platformIndex, &platformIndex);
+        op.add<popl::Value<int>>("d", "device", "Device Index", deviceIndex, &deviceIndex);
+
+        bool printUsage = false;
+        try {
+            op.parse(argc, argv);
+        } catch (std::exception& e) {
+            fprintf(stderr, "Error: %s\n\n", e.what());
+            printUsage = true;
+        }
+
+        if (printUsage || !op.unknown_options().empty() || !op.non_option_args().empty()) {
+            fprintf(stderr,
+                "Usage: copybufferkernel [options]\n"
+                "%s", op.help().c_str());
+            return -1;
+        }
+    }
+
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+
+    printf("Running on platform: %s\n",
+        platforms[platformIndex].getInfo<CL_PLATFORM_NAME>().c_str() );
+
+    std::vector<cl::Device> devices;
+    platforms[platformIndex].getDevices(CL_DEVICE_TYPE_ALL, &devices);
+
+    printf("Running on device: %s\n",
+        devices[deviceIndex].getInfo<CL_DEVICE_NAME>().c_str() );
+
+    cl::Context context{devices[deviceIndex]};
+    cl::CommandQueue commandQueue = cl::CommandQueue{context, devices[deviceIndex]};
+
+    cl::Program program{ context, kernelString };
+    program.build();
+    cl::Kernel kernel = cl::Kernel{ program, "CopyBuffer" };
+
+    cl::Buffer deviceMemSrc = cl::Buffer{
+        context,
+        CL_MEM_ALLOC_HOST_PTR,
+        gwx * sizeof( cl_uint ) };
+
+    cl::Buffer deviceMemDst = cl::Buffer{
+        context,
+        CL_MEM_ALLOC_HOST_PTR,
+        gwx * sizeof( cl_uint ) };
+
+    // initialization
+    {
+        cl_uint*    pSrc = (cl_uint*)commandQueue.enqueueMapBuffer(
+            deviceMemSrc,
+            CL_TRUE,
+            CL_MAP_WRITE_INVALIDATE_REGION,
+            0,
+            gwx * sizeof(cl_uint) );
+
+        for( size_t i = 0; i < gwx; i++ )
+        {
+            pSrc[i] = (cl_uint)(i);
+        }
+
+        commandQueue.enqueueUnmapMemObject(
+            deviceMemSrc,
+            pSrc );
+    }
+
+    cl_command_buffer_khr cmdbuf = clCreateCommandBufferKHR(
+        1,
+        &commandQueue(),
+        NULL,
+        NULL);
+
+    kernel.setArg(0, deviceMemDst);
+    kernel.setArg(1, deviceMemSrc);
+    clCommandNDRangeKernelKHR(
+        cmdbuf,
+        NULL,
+        NULL,
+        kernel(),
+        1,
+        NULL,
+        &gwx,
+        NULL,
+        0,
+        NULL,
+        NULL,
+        NULL);
+    clFinalizeCommandBufferKHR(cmdbuf);
+
+    clEnqueueCommandBufferKHR(
+        0,
+        NULL,
+        cmdbuf,
+        0,
+        NULL,
+        NULL);
+
+    // verification
+    {
+        const cl_uint*  pDst = (const cl_uint*)commandQueue.enqueueMapBuffer(
+            deviceMemDst,
+            CL_TRUE,
+            CL_MAP_READ,
+            0,
+            gwx * sizeof(cl_uint) );
+
+        unsigned int    mismatches = 0;
+
+        for( size_t i = 0; i < gwx; i++ )
+        {
+            if( pDst[i] != i )
+            {
+                if( mismatches < 16 )
+                {
+                    fprintf(stderr, "MisMatch!  dst[%d] == %08X, want %08X\n",
+                        (unsigned int)i,
+                        pDst[i],
+                        (unsigned int)i );
+                }
+                mismatches++;
+            }
+        }
+
+        if( mismatches )
+        {
+            fprintf(stderr, "Error: Found %d mismatches / %d values!!!\n",
+                mismatches,
+                (unsigned int)gwx );
+        }
+        else
+        {
+            printf("Success.\n");
+        }
+
+        commandQueue.enqueueUnmapMemObject(
+            deviceMemDst,
+            (void*)pDst );
+    }
+
+    return 0;
+}
