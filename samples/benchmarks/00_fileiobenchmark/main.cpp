52682cc143fd7e84d69a2d00b754aba4b0585031
Ben Ashbaugh <ben.ashbaugh@intel.com>
2022-03-31 08:49:22 -0700

    add text file fixture

--- samples/benchmarks/00_fileiobenchmark/main.cpp
+++ samples/benchmarks/00_fileiobenchmark/main.cpp
@@ -36,12 +36,18 @@
 #define GET_PROCESS_ID()    GetCurrentProcessId()
 #define GET_THREAD_ID()     GetCurrentThreadId()
 
+#define CLI_SPRINTF(_s, _sz, _f, ...)   sprintf_s(_s, _TRUNCATE, _f, ##__VA_ARGS__)
+#define CLI_VSPRINTF(_s, _sz, _f, _a)   vsnprintf_s(_s, _TRUNCATE, _f, _a)
+
 #else
 
 #include <pthread.h>
 
 #define GET_PROCESS_ID()    getpid()
 #define GET_THREAD_ID()     pthread_self()
+
+#define CLI_SPRINTF(_s, _sz, _f, ...)   snprintf(_s, _sz, _f, ##__VA_ARGS__)
+#define CLI_VSPRINTF(_s, _sz, _f, _a)   vsnprintf(_s, _sz, _f, _a)
 
 #endif
 
@@ -50,24 +56,29 @@ using test_clock = std::chrono::steady_clock;
 struct ofstream_fixture : public benchmark::Fixture
 {
     std::ofstream   trace;
+    uint64_t    fixture_processId;
+    uint64_t    fixture_threadId;
     test_clock::time_point   global_start;
     test_clock::time_point   start;
     test_clock::time_point   end;
 
+    static const size_t BUFFER_SIZE = 16 * 1024;
+    char buffer[BUFFER_SIZE];
+
     virtual void SetUp(benchmark::State& state) override {
         global_start = test_clock::now();
 
-        trace.open( "dummy_trace_ofstream.json", std::ios::out );
-        trace << "[\n";
+        trace.open( "dummy_trace_ofstream.json", std::ios::out | std::ios::binary );
+        trace << "[ \n";
 
         start = test_clock::now();
 
-        uint64_t    processId = GET_PROCESS_ID();
-        uint64_t    threadId = GET_THREAD_ID();
+        fixture_processId = GET_PROCESS_ID();
+        fixture_threadId = GET_THREAD_ID();
         std::string processName = "dummy_process";
         trace
-            << "{\"ph\":\"M\", \"name\":\"process_name\", \"pid\":" << processId
-            << ", \"tid\":" << threadId
+            << "{\"ph\":\"M\", \"name\":\"process_name\", \"pid\":" << fixture_processId
+            << ", \"tid\":" << fixture_threadId
             << ", \"args\":{\"name\":\"" << processName
             << "\"}},\n";
 
@@ -82,7 +93,7 @@ BENCHMARK_DEFINE_F(ofstream_fixture, ChromeCallLogging_WithTimes_WithArgs)(bench
 {
     while(state.KeepRunning()) {
         std::string name;
-        name += __FUNCTION__;
+        name += "test_function";
 
         std::ostringstream args;
 
@@ -110,7 +121,7 @@ BENCHMARK_REGISTER_F(ofstream_fixture, ChromeCallLogging_WithTimes_WithArgs);
 BENCHMARK_DEFINE_F(ofstream_fixture, ChromeCallLogging_WithTimes_WithArgs_init)(benchmark::State& state)
 {
     while(state.KeepRunning()) {
-        std::string name(__FUNCTION__);
+        std::string name("test_function");
 
         std::ostringstream args;
 
@@ -138,7 +149,7 @@ BENCHMARK_REGISTER_F(ofstream_fixture, ChromeCallLogging_WithTimes_WithArgs_init
 BENCHMARK_DEFINE_F(ofstream_fixture, ChromeCallLogging_WithTimes_WithArgs_nospaces)(benchmark::State& state)
 {
     while(state.KeepRunning()) {
-        std::string name(__FUNCTION__);
+        std::string name("test_function");
 
         std::ostringstream args;
 
@@ -163,27 +174,124 @@ BENCHMARK_DEFINE_F(ofstream_fixture, ChromeCallLogging_WithTimes_WithArgs_nospac
 }
 BENCHMARK_REGISTER_F(ofstream_fixture, ChromeCallLogging_WithTimes_WithArgs_nospaces);
 
-struct FILE_fixture : public benchmark::Fixture
+BENCHMARK_DEFINE_F(ofstream_fixture, ChromeCallLogging_WithTimes_WithArgs_nospaces_fixturepid)(benchmark::State& state)
 {
-    FILE*   trace;
+    while(state.KeepRunning()) {
+        std::string name("test_function");
+
+        std::ostringstream args;
+
+        uint64_t    threadId = GET_THREAD_ID();
+
+        using us = std::chrono::microseconds;
+        uint64_t    usStart =
+            std::chrono::duration_cast<us>(start - global_start).count();
+        uint64_t    usDelta =
+            std::chrono::duration_cast<us>(end - start).count();
+
+        trace
+            << "{\"ph\":\"X\",\"pid\":" << fixture_processId
+            << ",\"tid\":" << threadId
+            << ",\"name\":\"" << name
+            << "\",\"ts\":" << usStart
+            << ",\"dur\":" << usDelta
+            << args.str()
+            << "},\n";
+    }
+}
+BENCHMARK_REGISTER_F(ofstream_fixture, ChromeCallLogging_WithTimes_WithArgs_nospaces_fixturepid);
+
+struct ofstream_text_fixture : public benchmark::Fixture
+{
+    std::ofstream   trace;
+    uint64_t    fixture_processId;
+    uint64_t    fixture_threadId;
     test_clock::time_point   global_start;
     test_clock::time_point   start;
     test_clock::time_point   end;
+
+    static const size_t BUFFER_SIZE = 16 * 1024;
+    char buffer[BUFFER_SIZE];
+
+    virtual void SetUp(benchmark::State& state) override {
+        global_start = test_clock::now();
+
+        trace.open( "dummy_trace_ofstream.json", std::ios::out );
+        trace << "[ \n";
+
+        start = test_clock::now();
+
+        fixture_processId = GET_PROCESS_ID();
+        fixture_threadId = GET_THREAD_ID();
+        std::string processName = "dummy_process";
+        trace
+            << "{\"ph\":\"M\", \"name\":\"process_name\", \"pid\":" << fixture_processId
+            << ", \"tid\":" << fixture_threadId
+            << ", \"args\":{\"name\":\"" << processName
+            << "\"}},\n";
+
+        end = test_clock::now();
+    }
+    virtual void TearDown(benchmark::State& state) override {
+        trace.close();
+    }
+};
+
+BENCHMARK_DEFINE_F(ofstream_text_fixture, ChromeCallLogging_WithTimes_WithArgs)(benchmark::State& state)
+{
+    while(state.KeepRunning()) {
+        std::string name;
+        name += "test_function";
+
+        std::ostringstream args;
+
+        uint64_t    processId = GET_PROCESS_ID();
+        uint64_t    threadId = GET_THREAD_ID();
+
+        using us = std::chrono::microseconds;
+        uint64_t    usStart =
+            std::chrono::duration_cast<us>(start - global_start).count();
+        uint64_t    usDelta =
+            std::chrono::duration_cast<us>(end - start).count();
+
+        trace
+            << "{\"ph\":\"X\", \"pid\":" << processId
+            << ", \"tid\":" << threadId
+            << ", \"name\":\"" << name
+            << "\", \"ts\":" << usStart
+            << ", \"dur\":" << usDelta
+            << args.str()
+            << "},\n";
+    }
+}
+BENCHMARK_REGISTER_F(ofstream_text_fixture, ChromeCallLogging_WithTimes_WithArgs);
+
+struct FILE_fixture : public benchmark::Fixture
+{
+    FILE*   trace;
+    uint64_t    fixture_processId;
+    uint64_t    fixture_threadId;
+    test_clock::time_point   global_start;
+    test_clock::time_point   start;
+    test_clock::time_point   end;
+
+    static const size_t BUFFER_SIZE = 16 * 1024;
+    char buffer[BUFFER_SIZE];
 
     virtual void SetUp(benchmark::State& state) override {
         global_start = test_clock::now();
 
         trace = fopen( "dummy_trace_FILE.json", "wb" );
-        fprintf(trace, "[\n");
+        fprintf(trace, "[ \n");
 
         start = test_clock::now();
 
-        uint64_t    processId = GET_PROCESS_ID();
-        uint64_t    threadId = GET_THREAD_ID();
+        fixture_processId = GET_PROCESS_ID();
+        fixture_threadId = GET_THREAD_ID();
         std::string processName = "dummy_process";
         fprintf(trace, "{\"ph\":\"M\", \"name\":\"process_name\", \"pid\":%" PRIu64 ", \"tid\":%" PRIu64 ", \"args\":{\"name\":\"%s\"}},\n",
-            processId,
-            threadId,
+            fixture_processId,
+            fixture_threadId,
             processName.c_str() );
 
         end = test_clock::now();
@@ -197,7 +305,7 @@ BENCHMARK_DEFINE_F(FILE_fixture, ChromeCallLogging_WithTimes_WithArgs)(benchmark
 {
     while(state.KeepRunning()) {
         std::string name;
-        name += __FUNCTION__;
+        name += "test_function";
 
         std::ostringstream args;
 
@@ -225,7 +333,7 @@ BENCHMARK_DEFINE_F(FILE_fixture, ChromeCallLogging_WithTimes_WithArgs_nospaces)(
 {
     while(state.KeepRunning()) {
         std::string name;
-        name += __FUNCTION__;
+        name += "test_function";
 
         std::ostringstream args;
 
@@ -248,6 +356,93 @@ BENCHMARK_DEFINE_F(FILE_fixture, ChromeCallLogging_WithTimes_WithArgs_nospaces)(
     }
 }
 BENCHMARK_REGISTER_F(FILE_fixture, ChromeCallLogging_WithTimes_WithArgs_nospaces);
+
+BENCHMARK_DEFINE_F(FILE_fixture, ChromeCallLogging_WithTimes_WithArgs_nospaces_fixturepid)(benchmark::State& state)
+{
+    while(state.KeepRunning()) {
+        std::string name;
+        name += "test_function";
+
+        std::ostringstream args;
+
+        uint64_t    threadId = GET_THREAD_ID();
+
+        using us = std::chrono::microseconds;
+        uint64_t    usStart =
+            std::chrono::duration_cast<us>(start - global_start).count();
+        uint64_t    usDelta =
+            std::chrono::duration_cast<us>(end - start).count();
+
+        fprintf(trace, "{\"ph\":\"X\",\"pid\":%" PRIu64 ",\"tid\":%" PRIu64 ",\"name\":\"%s\",\"ts\":%" PRIu64 ",\"dur\":%" PRIu64 "%s},\n",
+            fixture_processId,
+            threadId,
+            name.c_str(),
+            usStart,
+            usDelta,
+            args.str().c_str() );
+    }
+}
+BENCHMARK_REGISTER_F(FILE_fixture, ChromeCallLogging_WithTimes_WithArgs_nospaces_fixturepid);
+
+BENCHMARK_DEFINE_F(FILE_fixture, ChromeCallLogging_WithTimes_WithArgs_nospaces_sprintf_fprintf)(benchmark::State& state)
+{
+    while(state.KeepRunning()) {
+        std::string name;
+        name += "test_function";
+
+        std::ostringstream args;
+
+        uint64_t    processId = GET_PROCESS_ID();
+        uint64_t    threadId = GET_THREAD_ID();
+
+        using us = std::chrono::microseconds;
+        uint64_t    usStart =
+            std::chrono::duration_cast<us>(start - global_start).count();
+        uint64_t    usDelta =
+            std::chrono::duration_cast<us>(end - start).count();
+
+        int size = CLI_SPRINTF(buffer, BUFFER_SIZE,
+            "{\"ph\":\"X\",\"pid\":%" PRIu64 ",\"tid\":%" PRIu64 ",\"name\":\"%s\",\"ts\":%" PRIu64 ",\"dur\":%" PRIu64 "%s},\n",
+            processId,
+            threadId,
+            name.c_str(),
+            usStart,
+            usDelta,
+            args.str().c_str() );
+        fprintf(trace, "%s", buffer);
+    }
+}
+BENCHMARK_REGISTER_F(FILE_fixture, ChromeCallLogging_WithTimes_WithArgs_nospaces_sprintf_fprintf);
+
+BENCHMARK_DEFINE_F(FILE_fixture, ChromeCallLogging_WithTimes_WithArgs_nospaces_sprintf_fwrite)(benchmark::State& state)
+{
+    while(state.KeepRunning()) {
+        std::string name;
+        name += "test_function";
+
+        std::ostringstream args;
+
+        uint64_t    processId = GET_PROCESS_ID();
+        uint64_t    threadId = GET_THREAD_ID();
+
+        using us = std::chrono::microseconds;
+        uint64_t    usStart =
+            std::chrono::duration_cast<us>(start - global_start).count();
+        uint64_t    usDelta =
+            std::chrono::duration_cast<us>(end - start).count();
+
+        int size = CLI_SPRINTF(buffer, BUFFER_SIZE,
+            "{\"ph\":\"X\",\"pid\":%" PRIu64 ",\"tid\":%" PRIu64 ",\"name\":\"%s\",\"ts\":%" PRIu64 ",\"dur\":%" PRIu64 "%s},\n",
+            processId,
+            threadId,
+            name.c_str(),
+            usStart,
+            usDelta,
+            args.str().c_str() );
+        fwrite(buffer, size, 1, trace);
+    }
+}
+BENCHMARK_REGISTER_F(FILE_fixture, ChromeCallLogging_WithTimes_WithArgs_nospaces_sprintf_fwrite);
 
 
 BENCHMARK_MAIN();
