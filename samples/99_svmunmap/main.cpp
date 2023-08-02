3a82dd01bfcef38968afb523307778aca9ad01f8
Ben Ashbaugh <ben.ashbaugh@intel.com>
2023-08-01 22:17:50 -0700

    add an SVM unmap test
    see: https://github.com/KhronosGroup/OpenCL-Docs/issues/890

---
+++ samples/99_svmunmap/main.cpp
@@ -0,0 +1,67 @@
+/*
+// Copyright (c) 2023 Ben Ashbaugh
+//
+// SPDX-License-Identifier: MIT
+*/
+
+#include <popl/popl.hpp>
+
+#include <CL/opencl.hpp>
+
+int main(int argc, char** argv)
+{
+    int platformIndex = 0;
+    int deviceIndex = 0;
+
+    {
+        popl::OptionParser op("Supported Options");
+        op.add<popl::Value<int>>("p", "platform", "Platform Index", platformIndex, &platformIndex);
+        op.add<popl::Value<int>>("d", "device", "Device Index", deviceIndex, &deviceIndex);
+
+        bool printUsage = false;
+        try {
+            op.parse(argc, argv);
+        } catch (std::exception& e) {
+            fprintf(stderr, "Error: %s\n\n", e.what());
+            printUsage = true;
+        }
+
+        if (printUsage || !op.unknown_options().empty() || !op.non_option_args().empty()) {
+            fprintf(stderr,
+                "Usage: copybuffer [options]\n"
+                "%s", op.help().c_str());
+            return -1;
+        }
+    }
+
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+
+    printf("Running on platform: %s\n",
+        platforms[platformIndex].getInfo<CL_PLATFORM_NAME>().c_str() );
+
+    std::vector<cl::Device> devices;
+    platforms[platformIndex].getDevices(CL_DEVICE_TYPE_ALL, &devices);
+
+    printf("Running on device: %s\n",
+        devices[deviceIndex].getInfo<CL_DEVICE_NAME>().c_str() );
+
+    cl::Context context{devices[deviceIndex]};
+    cl::CommandQueue commandQueue = cl::CommandQueue{context, devices[deviceIndex]};
+
+    void* ptr = clSVMAlloc(context(), CL_MEM_READ_WRITE, 1024, 0);
+    printf("clSVMAlloc returned %p\n", ptr);
+
+    cl_event event = nullptr;
+    cl_int errorCode = clEnqueueSVMUnmap(
+        commandQueue(),
+        ptr,
+        0,
+        nullptr,
+        &event);
+    printf("clEnqueueSVMUnmap returned %d, event is %p.\n", errorCode, event);
+    clReleaseEvent(event);
+
+    clSVMFree(context(), ptr);
+    return 0;
+}
