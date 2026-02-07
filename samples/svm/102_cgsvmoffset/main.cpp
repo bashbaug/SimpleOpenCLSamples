0e8c784881f1f942748f41996e209fffd9e5547b
Ben Ashbaugh <ben.ashbaugh@intel.com>
2026-02-06 17:04:41 -0800

    add SVM offset examples

---
+++ samples/svm/102_cgsvmoffset/main.cpp
@@ -0,0 +1,192 @@
+/*
+// Copyright (c) 2026 Ben Ashbaugh
+//
+// SPDX-License-Identifier: MIT
+*/
+
+#include <memory>
+#include <popl/popl.hpp>
+
+#include <CL/opencl.hpp>
+
+#include "util.hpp"
+
+const size_t    gwx = 1024*1024;
+
+static const char kernelString[] = R"CLC(
+kernel void CopyBuffer( global uint* dst, global uint* src )
+{
+    uint id = get_global_id(0);
+    dst[id] = src[id];
+}
+)CLC";
+
+struct SVMDeleter
+{
+    SVMDeleter(cl::Context& _c) : context(_c) {}
+    void operator()(void* ptr) {
+        clSVMFree(context(), ptr);
+    }
+    cl::Context context;
+};
+
+int main(
+    int argc,
+    char** argv )
+{
+    int platformIndex = 0;
+    int deviceIndex = 0;
+
+    {
+        popl::OptionParser op("Supported Options");
+        op.add<popl::Value<int>>("p", "platform", "Platform Index", platformIndex, &platformIndex);
+        op.add<popl::Value<int>>("d", "device", "Device Index", deviceIndex, &deviceIndex);
+        bool printUsage = false;
+        try {
+            op.parse(argc, argv);
+        } catch (std::exception& e) {
+            fprintf(stderr, "Error: %s\n\n", e.what());
+            printUsage = true;
+        }
+
+        if (printUsage || !op.unknown_options().empty() || !op.non_option_args().empty()) {
+            fprintf(stderr,
+                "Usage: dmemhelloworld [options]\n"
+                "%s", op.help().c_str());
+            return -1;
+        }
+    }
+
+    std::vector<cl::Platform> platforms;
+    cl::Platform::get(&platforms);
+
+    if (!checkPlatformIndex(platforms, platformIndex)) {
+        return -1;
+    }
+
+    printf("Running on platform: %s\n",
+        platforms[platformIndex].getInfo<CL_PLATFORM_NAME>().c_str() );
+
+    std::vector<cl::Device> devices;
+    platforms[platformIndex].getDevices(CL_DEVICE_TYPE_ALL, &devices);
+
+    printf("Running on device: %s\n",
+        devices[deviceIndex].getInfo<CL_DEVICE_NAME>().c_str() );
+
+    cl_device_svm_capabilities svmcaps = devices[deviceIndex].getInfo<CL_DEVICE_SVM_CAPABILITIES>();
+    if( svmcaps & CL_DEVICE_SVM_COARSE_GRAIN_BUFFER ) {
+        printf("Device supports CL_DEVICE_SVM_COARSE_GRAIN_BUFFER.\n");
+    } else {
+        printf("Device does not support CL_DEVICE_SVM_COARSE_GRAIN_BUFFER, exiting.\n");
+        return -1;
+    }
+
+    cl::Context context{devices[deviceIndex]};
+    cl::CommandQueue commandQueue{context, devices[deviceIndex]};
+
+    cl::Program program{ context, kernelString };
+    program.build();
+    cl::Kernel kernel = cl::Kernel{ program, "CopyBuffer" };
+
+    {
+        constexpr size_t count = gwx + 4;
+
+        std::unique_ptr<cl_int[], SVMDeleter> src(
+            (cl_int*)clSVMAlloc(
+                context(),
+                CL_MEM_READ_WRITE,
+                count * sizeof(cl_int),
+                0),
+            SVMDeleter(context));
+        std::unique_ptr<cl_int[], SVMDeleter> dst(
+            (cl_int*)clSVMAlloc(
+                context(),
+                CL_MEM_READ_WRITE,
+                count * sizeof(cl_int),
+                0),
+            SVMDeleter(context));
+
+        if (src && dst) {
+            // initialization
+            {
+                commandQueue.enqueueMapSVM(
+                    src,
+                    CL_TRUE,
+                    CL_MAP_WRITE_INVALIDATE_REGION,
+                    count * sizeof(cl_int) );
+                for( size_t i = 0; i < count; i++ )
+                {
+                    auto val = static_cast<cl_int>(i);
+                    src[i] = val;
+                    dst[i] = -val;
+                }
+
+                commandQueue.enqueueUnmapSVM( src );
+            }
+
+            // execution
+            constexpr size_t dstOffset = 1;
+            constexpr size_t srcOffset = 2;
+            printf("Setting dst argument with offset: %zu\n", dstOffset);
+            kernel.setArg( 0, dst.get() + dstOffset );
+            printf("Setting src argument with offset: %zu\n", srcOffset);
+            kernel.setArg( 1, src.get() + srcOffset );
+            commandQueue.enqueueNDRangeKernel(
+                kernel,
+                cl::NullRange,
+                cl::NDRange{gwx} );
+
+            // verification
+            {
+                commandQueue.enqueueMapSVM(
+                    dst,
+                    CL_TRUE,
+                    CL_MAP_READ,
+                    count * sizeof(cl_uint) );
+
+                size_t mismatches = 0;
+
+                for (size_t i = 0; i < dstOffset; i++) {
+                    if (dst[i] != i) {
+                        fprintf(stderr, "MisMatch at dst buffer start!  dst[%zu] = %08X, want %08X\n",
+                            i,
+                            dst[i],
+                            (cl_int)i );
+                        mismatches++;
+                    }
+                }
+
+
+                for (size_t i = 0; i < gwx; i++) {
+                    auto ci = i + dstOffset;
+                    auto cv = i + srcOffset;
+                    if (dst[ci] != cv) {
+                        if (mismatches < 16) {
+                            fprintf(stderr, "MisMatch!  dst[%zu] == %08X, want %08X\n",
+                                ci,
+                                dst[ci],
+                                (cl_int)cv );
+                        }
+                        mismatches++;
+                    }
+                }
+
+                commandQueue.enqueueUnmapSVM( dst );
+
+                if (mismatches) {
+                    fprintf(stderr, "Error: Found %zu mismatches / %zu values!!!\n",
+                        mismatches,
+                        count );
+                } else {
+                    printf("Success.\n");
+                }
+            }
+        } else {
+            printf("Allocation failed - does this device support SVM?\n");
+        }
+
+        printf("Cleaning up...\n");
+    }
+
+    return 0;
+}
